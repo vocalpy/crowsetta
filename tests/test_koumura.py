b7a0c48edbd2ed40fc95e9ceedffb544e4a5c57c
nickledave <nickledave@users.noreply.github.com>
2018-12-09 22:22:50 -0500

    change tests to reflect refactor, add test_koumura.py and test_notmat.py

--- tests/test_koumura.py
+++ tests/test_koumura.py
@@ -18,11 +18,11 @@ ANNOT_DICT_FIELDNAMES = {'filename': str,
                          'offsets_s': np.ndarray,
                          'labels': np.ndarray}
 
-SYL_DICT_FIELDNAMES = ['filename', 
-                       'onset_Hz', 
-                       'offset_Hz', 
-                       'onset_s', 
-                       'offset_s', 
+SYL_DICT_FIELDNAMES = ['filename',
+                       'onset_Hz',
+                       'offset_Hz',
+                       'onset_s',
+                       'offset_s',
                        'label']
 
 
@@ -33,6 +33,16 @@ class TestAnnotation(unittest.TestCase):
 
     def tearDown(self):
         shutil.rmtree(self.tmp_output_dir)
+
+    def test_notmat_to_annot_dict(self):
+        notmat = os.path.join(self.test_data_dir,
+                              os.path.normpath(
+                                  'cbins/gy6or6/032312/'
+                                  'gy6or6_baseline_230312_0808.138.cbin.not.mat'))
+        annot_dict = conbirt.notmat_to_annot_dict(notmat)
+        for fieldname, fieldtype in ANNOT_DICT_FIELDNAMES.items():
+            assert fieldname in annot_dict
+            assert type(annot_dict[fieldname]) == fieldtype
 
     def test_annot_list_to_csv(self):
         # compare csv created by annot_list_to_csv
@@ -68,6 +78,39 @@ class TestAnnotation(unittest.TestCase):
                 compare_rows.append(row)
         for test_row, compare_row in zip(test_rows, compare_rows):
             assert test_row == compare_row
+
+    def test_notmat_list_to_csv(self):
+        # since notmat_list_to_csv is basically a wrapper around
+        # notmat_to_annot_dict and annot_list_to_csv,
+        # and those are tested above,
+        # here just need to make sure this function doesn't fail
+        cbin_dir = os.path.join(self.test_data_dir,
+                                os.path.normpath('cbins/gy6or6/032312/'))
+        notmat_list = glob(os.path.join(cbin_dir, '*.not.mat'))
+        # below, sorted() so it's the same order on different platforms
+        notmat_list = sorted(notmat_list)
+        csv_filename = os.path.join(str(self.tmp_output_dir),
+                                    'test.csv')
+        conbirt.notmat_list_to_csv(notmat_list, csv_filename)
+        # make sure file was created
+        assert os.path.isfile(csv_filename)
+
+        # to be extra sure, make sure all filenames from
+        # .not.mat list are in csv
+        filenames_from_csv = []
+        with open(csv_filename, 'r', newline='') as csvfile:
+            reader = csv.DictReader(csvfile, fieldnames=SYL_DICT_FIELDNAMES)
+            header = next(reader)
+            for row in reader:
+                filenames_from_csv.append(row['filename'])
+        for fname_from_csv in filenames_from_csv:
+            assert(fname_from_csv + '.not.mat' in notmat_list)
+
+    def test_make_notmat(self):
+        assert False
+
+    def test_load_annotation_csv(self):
+        assert False
 
     def test_csv_to_annot_list(self):
         csv_fname = os.path.join(self.test_data_dir,
---
+++ tests/test_notmat.py
@@ -0,0 +1,79 @@
+import os
+from glob import glob
+import tempfile
+import shutil
+import csv
+import unittest
+
+import numpy as np
+
+import conbirt
+
+TESTS_DIR = os.path.dirname(os.path.abspath(__file__))
+
+ANNOT_DICT_FIELDNAMES = {'filename': str,
+                         'onsets_Hz': np.ndarray,
+                         'offsets_Hz': np.ndarray,
+                         'onsets_s': np.ndarray,
+                         'offsets_s': np.ndarray,
+                         'labels': np.ndarray}
+
+SYL_DICT_FIELDNAMES = ['filename',
+                       'onset_Hz',
+                       'offset_Hz',
+                       'onset_s',
+                       'offset_s',
+                       'label']
+
+
+class TestAnnotation(unittest.TestCase):
+    def setUp(self):
+        self.test_data_dir = os.path.join(TESTS_DIR, 'test_data')
+        self.tmp_output_dir = tempfile.mkdtemp()
+
+    def tearDown(self):
+        shutil.rmtree(self.tmp_output_dir)
+
+    def test_notmat2seq(self):
+        notmat = os.path.join(self.test_data_dir,
+                              os.path.normpath(
+                                  'cbins/gy6or6/032312/'
+                                  'gy6or6_baseline_230312_0808.138.cbin.not.mat'))
+        annot_dict = conbirt.notmat.notmat2seq(notmat)
+        for fieldname, fieldtype in ANNOT_DICT_FIELDNAMES.items():
+            assert fieldname in annot_dict
+            assert type(annot_dict[fieldname]) == fieldtype
+
+    def test_notmat_list_to_csv(self):
+        # since notmat_list_to_csv is basically a wrapper around
+        # notmat_to_annot_dict and annot_list_to_csv,
+        # and those are tested above,
+        # here just need to make sure this function doesn't fail
+        cbin_dir = os.path.join(self.test_data_dir,
+                                os.path.normpath('cbins/gy6or6/032312/'))
+        notmat_list = glob(os.path.join(cbin_dir, '*.not.mat'))
+        # below, sorted() so it's the same order on different platforms
+        notmat_list = sorted(notmat_list)
+        csv_filename = os.path.join(str(self.tmp_output_dir),
+                                    'test.csv')
+        conbirt.notmat.notmat_list_to_csv(notmat_list, csv_filename)
+        # make sure file was created
+        assert os.path.isfile(csv_filename)
+
+        # to be extra sure, make sure all filenames from
+        # .not.mat list are in csv
+        filenames_from_csv = []
+        with open(csv_filename, 'r', newline='') as csvfile:
+            reader = csv.DictReader(csvfile, fieldnames=SYL_DICT_FIELDNAMES)
+            header = next(reader)
+            for row in reader:
+                filenames_from_csv.append(row['filename'])
+        for fname_from_csv in filenames_from_csv:
+            assert(fname_from_csv + '.not.mat' in notmat_list)
+
+    def test_make_notmat(self):
+        assert False
+
+
+if __name__ == '__main__':
+    unittest.main()
