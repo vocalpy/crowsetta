093a16bbf90823483643fc9a01305477aac9a570
nickledave <nickledave@users.noreply.github.com>
2018-12-09 22:22:33 -0500

    change src/conbirt/__init__.py to reflect refactor

--- src/conbirt/__init__.py
+++ src/conbirt/__init__.py
@@ -1,3 +1,4 @@
-from .conbirt import notmat_to_annot_dict, notmat_list_to_csv, make_notmat
-from .conbirt import koumura_xml_to_annot_list, annot_list_to_csv, xml_to_annot_list
-from .conbirt import xml_to_csv, csv_to_annot_list
+from . import notmat
+from . import koumura
+from . import conbirt
+from . import tuples
