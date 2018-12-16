661ba29a770bb6d0136c4cb2939a108f2bfbddc8
nickledave <nickledave@users.noreply.github.com>
2018-12-16 11:26:59 -0500

    fix/finish writing src/conbirt/notmat.py tests

--- tests/test_notmat.py
+++ tests/test_notmat.py
@@ -4,14 +4,16 @@ import tempfile
 import shutil
 import csv
 import unittest
+from pathlib import Path, PureWindowsPath
 
 import numpy as np
+import evfuncs
 
 import conbirt
 
 TESTS_DIR = os.path.dirname(os.path.abspath(__file__))
 
-ANNOT_DICT_FIELDNAMES = {'filename': str,
+ANNOT_DICT_FIELDNAMES = {'file': str,
                          'onsets_Hz': np.ndarray,
                          'offsets_Hz': np.ndarray,
                          'onsets_s': np.ndarray,
@@ -39,15 +41,15 @@ class TestNotmat(unittest.TestCase):
                               os.path.normpath(
                                   'cbins/gy6or6/032312/'
                                   'gy6or6_baseline_230312_0808.138.cbin.not.mat'))
-        annot_dict = conbirt.notmat.notmat2seq(notmat)
+        seq = conbirt.notmat.notmat2seq(notmat)
         for fieldname, fieldtype in ANNOT_DICT_FIELDNAMES.items():
-            assert fieldname in annot_dict
-            assert type(annot_dict[fieldname]) == fieldtype
+            self.assertTrue(hasattr(seq, fieldname))
+            self.assertTrue(type(getattr(seq, fieldname)) == fieldtype)
 
     def test_notmat_list_to_csv(self):
         # since notmat_list_to_csv is basically a wrapper around
-        # notmat_to_annot_dict and annot_list_to_csv,
-        # and those are tested above,
+        # notmat2seq and seq2csv,
+        # and those are tested above and in other test modules,
         # here just need to make sure this function doesn't fail
         cbin_dir = os.path.join(self.test_data_dir,
                                 os.path.normpath('cbins/gy6or6/032312/'))
@@ -72,7 +74,41 @@ class TestNotmat(unittest.TestCase):
             assert(fname_from_csv + '.not.mat' in notmat_list)
 
     def test_make_notmat(self):
-        assert False
+        cbin_dir = os.path.join(self.test_data_dir,
+                                os.path.normpath('cbins/gy6or6/032312/'))
+        notmat_list = glob(os.path.join(cbin_dir, '*.not.mat'))
+        for notmat in notmat_list:
+            seq = conbirt.notmat.notmat2seq(notmat)
+            notmat_dict = evfuncs.load_notmat(notmat)
+            conbirt.notmat.make_notmat(filename=seq.file,
+                                       onsets_Hz=seq.onsets_Hz,
+                                       offsets_Hz=seq.offsets_Hz,
+                                       labels=np.asarray(list(notmat_dict['labels'])),
+                                       samp_freq=notmat_dict['Fs'],
+                                       threshold=notmat_dict['threshold'],
+                                       min_syl_dur=notmat_dict['min_dur']/1000,
+                                       min_silent_dur=notmat_dict['min_int']/1000,
+                                       alternate_path=self.tmp_output_dir,
+                                       other_vars=None
+                                       )
+            notmat_made = evfuncs.load_notmat(os.path.join(self.tmp_output_dir,
+                                                           os.path.basename(notmat)))
+            # can't do assert(new_dict == old_dict)
+            # because headers will be different (and we want them to be different)
+            for key in ['Fs', 'fname', 'labels', 'onsets', 'offsets',
+                        'min_int', 'min_dur', 'threshold', 'sm_win']:
+                if key == 'fname':
+                    # have to deal with Windows path saved in .not.mat files
+                    # and then compare file names without path to them
+                    notmat_dict_path = PureWindowsPath(notmat_dict[key])
+                    notmat_made_path = Path(notmat_made[key])
+                    self.assertTrue(notmat_dict_path.name == notmat_made_path.name)
+                elif type(notmat_dict[key]) == np.ndarray:
+                    self.assertTrue(np.allclose(notmat_dict[key],
+                                                notmat_made[key],
+                                                atol=1e-3, rtol=1e-3))
+                else:
+                    self.assertTrue(notmat_dict[key] == notmat_made[key])
 
 
 if __name__ == '__main__':
