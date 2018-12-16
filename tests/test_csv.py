f9b930f6cc522d3bf5c65ef87f87f36095dfb134
nickledave <nickledave@users.noreply.github.com>
2018-12-16 12:31:54 -0500

    fix tests for test_csv.py to test re-vamped functions; tests pass

--- tests/test_csv.py
+++ tests/test_csv.py
@@ -55,26 +55,28 @@ class TestAnnotation(unittest.TestCase):
         for test_row, compare_row in zip(test_rows, compare_rows):
             assert test_row == compare_row
 
-    def test_csv_to_annot_list(self):
+    def test_csv2seqlist(self):
         csv_fname = os.path.join(self.test_data_dir,
                                  os.path.normpath('csv/gy6or6_032312.csv'))
         # convert csv to conbirt list -- this is what we're testing
-        annot_list_from_csv = conbirt.csv.csv_to_annot_list(csv_fname)
+        seq_list_from_csv = conbirt.csv.csv2seqlist(csv_fname)
         cbin_dir = os.path.join(self.test_data_dir,
                                 os.path.normpath('cbins/gy6or6/032312/'))
 
-        # get what should be the same annotation list from .not.mat files
+        # get what should be the same seq list from .not.mat files
         # to compare with what we got from the csv
         notmat_list = glob(os.path.join(cbin_dir, '*.not.mat'))
         # below, sorted() so it's the same order on different platforms
         notmat_list = sorted(notmat_list)
-        annot_list_from_notmats = []
+        seq_list_from_notmats = []
         for notmat in notmat_list:
-            annot_list_from_notmats.append(conbirt.notmat_to_annot_dict(notmat,
-                                                                        basename=True))
+            seq_list_from_notmats.append(conbirt.notmat.notmat2seq(notmat,
+                                                                   basename=True))
 
         # make sure everything is the same in the two annotation lists
-        for from_csv, from_notmat in zip(annot_list_from_csv, annot_list_from_notmats):
+        for from_csv, from_notmat in zip(seq_list_from_csv, seq_list_from_notmats):
+            from_csv = from_csv._asdict()
+            from_notmat = from_notmat._asdict()
             for from_csv_key, from_csv_val in from_csv.items():
                 if type(from_csv_val) == str:
                     assert from_csv_val == from_notmat[from_csv_key]
