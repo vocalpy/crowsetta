936c03fcadd64fc7bd4890a17f44f18ed895f2e4
nickledave <nickledave@users.noreply.github.com>
2018-12-16 13:34:25 -0500

    add docstring for sequence that was in fomer conbirt.py module to src/conbirt/tuples.py

--- src/conbirt/tuples.py
+++ src/conbirt/tuples.py
@@ -1,3 +1,20 @@
+"""
+        each Sequence object has the following fields:
+            file : str
+                name of audio file with which annotation is associated.
+                See parameter abspath and basename below that allow for specifying how
+                filename is saved.
+            onsets_Hz : numpy.ndarray
+                of type int, onset of each annotated syllable in samples/second
+            offsets_Hz : numpy.ndarray
+                of type int, offset of each annotated syllable in samples/second
+            onsets_s : numpy.ndarray
+                of type float, onset of each annotated syllable in seconds
+            offsets_s : numpy.ndarray
+                of type float, offset of each annotated syllable in seconds
+            labels : numpy.ndarray
+                of type str, label for each annotated syllable
+"""
 from collections import namedtuple
 
 Sequence = namedtuple('Sequence', ['onsets_Hz',
