eeb41364251eb4aeea0f7d0dbbaa450a7c275676
nickledave <nickledave@users.noreply.github.com>
2018-12-16 19:03:00 -0500

    rewrite class and module docstrings in src/crowsetta/sequence.py

--- src/crowsetta/sequence.py
+++ src/crowsetta/sequence.py
@@ -1,20 +1,4 @@
-"""
-        each Sequence object has the following fields:
-            file : str
-                name of audio file with which annotation is associated.
-                See parameter abspath and basename below that allow for specifying how
-                filename is saved.
-            onsets_Hz : numpy.ndarray
-                of type int, onset of each annotated syllable in samples/second
-            offsets_Hz : numpy.ndarray
-                of type int, offset of each annotated syllable in samples/second
-            onsets_s : numpy.ndarray
-                of type float, onset of each annotated syllable in seconds
-            offsets_s : numpy.ndarray
-                of type float, offset of each annotated syllable in seconds
-            labels : numpy.ndarray
-                of type str, label for each annotated syllable
-"""
+"""defines Sequence object used for conversion"""
 from collections import namedtuple
 
 import numpy as np
@@ -22,21 +6,22 @@ import attr
 
 
 class Sequence:
-    """each Sequence object has the following fields:
+    """object that represents a sequence of segments, such as a bout of birdsong made
+    up of syllables, with the following fields:
         file : str
             name of audio file with which annotation is associated.
             See parameter abspath and basename below that allow for specifying how
             filename is saved.
         onsets_Hz : numpy.ndarray
-            of type int, onset of each annotated syllable in samples/second
+            of type int, onset of each annotated segment in samples/second
         offsets_Hz : numpy.ndarray
-            of type int, offset of each annotated syllable in samples/second
+            of type int, offset of each annotated segment in samples/second
         onsets_s : numpy.ndarray
-            of type float, onset of each annotated syllable in seconds
+            of type float, onset of each annotated segment in seconds
         offsets_s : numpy.ndarray
-            of type float, offset of each annotated syllable in seconds
+            of type float, offset of each annotated segment in seconds
         labels : numpy.ndarray
-            of type str, label for each annotated syllable
+            of type str, label for each annotated segment
     """
     def __init__(self, file, labels,
                  onsets_Hz=None, offsets_Hz=None,
