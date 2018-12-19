c4f1af1a781e2c0e009e1ae7b3e312960b72fc6e
nickledave <nickledave@users.noreply.github.com>
2018-12-19 14:09:45 -0500

    start new tests in tests/test_crowsetta.py

--- tests/test_crowsetta.py
+++ tests/test_crowsetta.py
@@ -1,17 +1,33 @@
 import os
+import shutil
+import tempfile
 import unittest
 
-import numpy as np
+import crowsetta
 
 TESTS_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
 class TestConbirt(unittest.TestCase):
     def setUp(self):
-        pass
+        self.tmp_output_dir = tempfile.mkdtemp()
+        self.test_data_dir = os.path.join(TESTS_DIR, 'test_data'
+        self.example_script_dir = os.path.join(TESTS_DIR,
+                                               '..', 'src', 'bin')
 
     def tearDown(self):
+        shutil.rmtree(self.tmp_output_dir)
+
+    def test_koumura_to_seq_defaults(self):
+        crow = Crowsetta()
+        xml_file = os.path.join(self.test_data_dir,
+                                'koumura', 'Bird0',
+                                'Annotation.xml')
+        seq = crow.to_seq(file=xml_file, file_format='koumura')
+
+    def test_koumura_to_csv(self):
         pass
 
-    def test_fromfunc(self):
+    def test_notmat_to_csv(self):
         pass
+
