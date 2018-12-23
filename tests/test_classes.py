35e453960772aff6be6c3386370a9c72cb63cf50
nickledave <nicholdav@gmail.com>
2018-12-23 14:03:48 -0500

    add tests/test_classes.py

---
+++ tests/test_classes.py
@@ -0,0 +1,17 @@
+import unittest
+
+import crowsetta.classes
+
+
+class Test_Classes(unittest.TestCase):
+    def test_Segment_init(self):
+        a_segment = crowsetta.classes.Segment
+        assert False
+
+    def test_Sequence_init(self):
+        a_sequence = crowsetta.classes.Sequence
+        assert False
+
+
+if __name__ == '__main__':
+    unittest.main()
