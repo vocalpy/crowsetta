14e76bfc3bd11065d2bc6d063290485fbb1c1a4d
nickledave <nicholdav@gmail.com>
2018-12-26 00:32:55 -0500

    add tests for to_dict method of Sequence in test_sequence.py

--- tests/test_sequence.py
+++ tests/test_sequence.py
@@ -146,5 +146,83 @@ class TestSequence(unittest.TestCase):
                                   offsets_Hz=np.asarray([0, 2, 4, 6, 8]),
                                   file='bird21.wav')
 
+    def test_to_dict_onset_offset_in_Hertz(self):
+        file = '0.wav'
+        labels = 'abcde'
+        onsets_Hz = np.asarray([0, 2, 4, 6, 8])
+        offsets_Hz = np.asarray([1, 3, 5, 7, 9])
+        seq = Sequence.from_keyword(labels=labels,
+                                    onsets_Hz=onsets_Hz,
+                                    offsets_Hz=offsets_Hz,
+                                    file=file)
+        seq_dict = seq.to_dict()
+
+        self.assertTrue(np.all(seq_dict['labels'] == np.asarray(list(labels))))
+        self.assertTrue(np.all(seq_dict['onsets_Hz'] == onsets_Hz))
+        self.assertTrue(np.all(seq_dict['offsets_Hz'] == offsets_Hz))
+        self.assertTrue(np.all(seq_dict['file'] == file))
+        self.assertTrue(seq_dict['onsets_s'] is None)
+        self.assertTrue(seq_dict['offsets_s'] is None)
+
+    def test_to_dict_onset_offset_in_seconds(self):
+        file = '0.wav'
+        labels = 'abcde'
+        onsets_s = np.asarray([0., 0.2, 0.4, 0.6, 0.8]),
+        offsets_s = np.asarray([0.1, 0.3, 0.5, 0.7, 0.9]),
+        seq = Sequence.from_keyword(labels=labels,
+                                    onsets_s=onsets_s,
+                                    offsets_s=offsets_s,
+                                    file=file)
+        seq_dict = seq.to_dict()
+
+        self.assertTrue(np.all(seq_dict['labels'] == np.asarray(list(labels))))
+        self.assertTrue(np.all(seq_dict['onsets_s'] == onsets_s))
+        self.assertTrue(np.all(seq_dict['offsets_s'] == offsets_s))
+        self.assertTrue(np.all(seq_dict['file'] == file))
+        self.assertTrue(seq_dict['onsets_Hz'] is None)
+        self.assertTrue(seq_dict['offsets_Hz'] is None)
+
+    def test_to_dict_onset_offset_both_units(self):
+        file = '0.wav'
+        labels = 'abcde'
+        onsets_Hz = np.asarray([0, 2, 4, 6, 8])
+        offsets_Hz = np.asarray([1, 3, 5, 7, 9])
+        onsets_s = np.asarray([0., 0.2, 0.4, 0.6, 0.8]),
+        offsets_s = np.asarray([0.1, 0.3, 0.5, 0.7, 0.9]),
+        seq = Sequence.from_keyword(labels=labels,
+                                    onsets_Hz=onsets_Hz,
+                                    offsets_Hz=offsets_Hz,
+                                    onsets_s=onsets_s,
+                                    offsets_s=offsets_s,
+                                    file=file)
+        seq_dict = seq.to_dict()
+
+        self.assertTrue(np.all(seq_dict['labels'] == np.asarray(list(labels))))
+        self.assertTrue(np.all(seq_dict['onsets_s'] == onsets_s))
+        self.assertTrue(np.all(seq_dict['offsets_s'] == offsets_s))
+        self.assertTrue(np.all(seq_dict['onsets_Hz'] == onsets_Hz))
+        self.assertTrue(np.all(seq_dict['offsets_Hz'] == offsets_Hz))
+        self.assertTrue(np.all(seq_dict['file'] == file))
+
+    def test_to_dict_with_bad_Segments_raises(self):
+        a_segment = Segment.from_keyword(
+            label='a',
+            onset_Hz=16000,
+            offset_Hz=32000,
+            file='bird21.wav'
+        )
+        list_of_segments = [a_segment] * 3
+        segment_with_different_file = Segment.from_keyword(
+            label='a',
+            onset_Hz=16000,
+            offset_Hz=32000,
+            file='bird12.wav'
+        )
+        list_of_segments.append(segment_with_different_file)
+        seq = Sequence(segments=list_of_segments)
+        with self.assertRaises(ValueError):
+            seq.to_dict()
+
+
 if __name__ == '__main__':
     unittest.main()
