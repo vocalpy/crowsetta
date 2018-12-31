742d9250c0bbbc2e67151d2acea08fe978940a72
nickledave <nickledave@users.noreply.github.com>
2018-12-31 14:09:55 -0500

    bump pre-release version to 0.2.0a4 in setup.py

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ URL = 'https://github.com/NickleDave/crowsetta'
 EMAIL = 'nicholdav@gmail.com'
 AUTHOR = 'David Nicholson'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.2.0a3'
+VERSION = '0.2.0a4'
 LICENSE = 'BSD'
 
 REQUIRED = [
