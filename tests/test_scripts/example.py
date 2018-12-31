a779e5e86727e3161b62674e578d6a9700dc2e02
nickledave <nickledave@users.noreply.github.com>
2018-12-30 22:48:45 -0500

    bring back example.py that was in src/bin, now have in tests/test_scripts/ to use with test_transcriber.py

---
+++ tests/test_scripts/example.py
@@ -0,0 +1,46 @@
+import numpy as np
+from scipy.io import loadmat
+
+from crowsetta.classes import Sequence
+
+
+def example2seq(file):
+    """example of a function that unpacks annotation from
+    a complicated data structure and returns the necessary
+    data from a Sequence object"""
+    mat = loadmat(file, squeeze_me=True)
+    seq_list = []
+    # annotation structure loads as a Python dictionary with two keys
+    # one maps to a list of filenames, 
+    # and the other to a Numpy array where each element is the annotation
+    # coresponding to the filename at the same index in the list.
+    # We can iterate over both by using the zip() function.
+    for filename, annotation in zip(mat['filenames'], mat['annotations']):
+        # below, .tolist() does not actually create a list,
+        # instead gets ndarray out of a zero-length ndarray of dtype=object.
+        # This is just weirdness that results from loading complicated data
+        # structure in .mat file.
+        onsets_s = annotation['segOnset'].tolist()
+        offsets_s = annotation['segOffset'].tolist()
+        labels = annotation['segType'].tolist()
+        if type(labels) == int:
+            # this happens when there's only one syllable in the file
+            # with only one corresponding label
+            labels = np.asarray([labels])  # so make it a one-element list
+        elif type(labels) == np.ndarray:
+            # this should happen whenever there's more than one label
+            pass
+        else:
+            # something unexpected happened
+            raise ValueError("Unable to load labels from {}, because "
+                             "the segType parsed as type {} which is "
+                             "not recognized.".format(wav_filename,
+                                                      type(labels)))
+        seq = Sequence(file=filename,
+                       labels=labels,
+                       onsets_s=onsets_s,
+                       offsets_s=offsets_s,
+                       onsets_Hz=None,
+                       offsets_Hz=None)
+        seq_list.append(seq)
+    return seq_list
