f5b46686694838ab1b0c05f24b6784c88b28826e
nickledave <nickledave@users.noreply.github.com>
2019-01-06 23:15:15 -0500

    fix transcriber tests for to_csv that broke after changing to_csv signature

--- tests/test_transcriber.py
+++ tests/test_transcriber.py
@@ -169,7 +169,7 @@ class TestTranscriber(unittest.TestCase):
                                   'example_user_format',
                                   'bird1_annotation.mat')
         with self.assertRaises(NotImplementedError):
-            scribe.to_csv(file=annotation, file_format='example', csv_fname='bad.csv')
+            scribe.to_csv(file=annotation, csv_filename='bad.csv', file_format='example')
 
     def test_call_to_format_when_None_raises(self):
         user_config = {
