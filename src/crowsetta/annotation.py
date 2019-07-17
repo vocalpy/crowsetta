cafcc6cc968f3a6fb01e9ce85045bfd1acd5add5
NickleDave <nickledave@users.noreply.github.com>
2019-07-17 11:04:54 -0500

    make audio_file attribute of Annotation optional

--- src/crowsetta/annotation.py
+++ src/crowsetta/annotation.py
@@ -7,6 +7,6 @@ from .sequence import Sequence
 @attr.s
 class Annotation:
     """a class to represent annotations for a single file"""
-    audio_file = attr.ib(validator=instance_of(str))
     annot_file = attr.ib(validator=instance_of(str))
+    audio_file = attr.ib(validator=optional(instance_of(str)), default=None)
     seq = attr.ib(validator=optional(instance_of(Sequence)), default=None)
