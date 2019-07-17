8a9003f2c51e725e5530370a0941afb22f6ef78b
NickleDave <nickledave@users.noreply.github.com>
2019-07-17 11:02:55 -0500

    change/modify Annotation attributes
    - 'file' is now 'audio_file', and add 'annot_file' attribute

--- src/crowsetta/annotation.py
+++ src/crowsetta/annotation.py
@@ -7,5 +7,6 @@ from .sequence import Sequence
 @attr.s
 class Annotation:
     """a class to represent annotations for a single file"""
-    file = attr.ib(validator=instance_of(str))
+    audio_file = attr.ib(validator=instance_of(str))
+    annot_file = attr.ib(validator=instance_of(str))
     seq = attr.ib(validator=optional(instance_of(Sequence)), default=None)
