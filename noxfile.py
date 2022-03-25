32576e12d468bcf21dbb8f2771a7c7c0b58d3160
David Nicholson <nickledave@users.noreply.github.com>
2022-03-24 21:58:16 -0400

    DEV: add two sessions to noxfile [skip ci]
    add 'dev' session that creates a virtual environment,
    adapted from cookbook:
    https://nox.thea.codes/en/stable/cookbook.html#instant-dev-environment
    
    and 'build' session,
    adapted from scikit-HEP
    https://scikit-hep.org/developer/tasks#build-pure-python

--- noxfile.py
+++ noxfile.py
@@ -1,8 +1,57 @@
+import os
+import pathlib
+import shutil
+
 import nox
+
+DIR = pathlib.Path(__file__).parent.resolve()
+VENV_DIR = pathlib.Path('./.venv').resolve()
+
+
+nox.options.sessions = ['tests', 'coverage']
 
 
 @nox.session
-def tests(session):
+def build(session: nox.Session) -> None:
+    """
+    Build an SDist and wheel with ``flit``.
+    """
+
+    dist_dir = DIR.joinpath("dist")
+    if dist_dir.exists():
+        shutil.rmtree(dist_dir)
+
+    session.install(".[dev]")
+    session.run("flit", "build")
+
+
+@nox.session
+def dev(session: nox.Session) -> None:
+    """
+    Sets up a python development environment for the project.
+
+    This session will:
+    - Create a python virtualenv for the session
+    - Install the `virtualenv` cli tool into this environment
+    - Use `virtualenv` to create a global project virtual environment
+    - Invoke the python interpreter from the global project environment to install
+      the project and all it's development dependencies.
+    """
+
+    session.install("virtualenv")
+    # VENV_DIR here is a pathlib.Path location of the project virtualenv
+    # e.g. .venv
+    session.run("virtualenv", os.fsdecode(VENV_DIR), silent=True)
+
+    python = os.fsdecode(VENV_DIR.joinpath("bin/python"))
+
+    # Use the venv's interpreter to install the project along with
+    # all it's dev dependencies, this ensures it's installed in the right way
+    session.run(python, "-m", "pip", "install", "-e", ".[dev,tests,doc]", external=True)
+
+
+@nox.session
+def tests(session) -> None:
     """
     Run the unit and regular tests.
     """
@@ -11,7 +60,7 @@ def tests(session):
 
 
 @nox.session
-def coverage(session):
+def coverage(session) -> None:
     """
     Run the unit and regular tests, and save coverage report
     """
