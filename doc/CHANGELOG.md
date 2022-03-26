d3c4f4c476481661e7042e802d3602e2acdd3449
David Nicholson <nickledave@users.noreply.github.com>
2022-03-26 10:16:23 -0400

    DEV: bump version to 3.4.0 [skip ci]

--- doc/CHANGELOG.md
+++ doc/CHANGELOG.md
@@ -4,7 +4,7 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
-## Unreleased
+## 3.4.0 -- 2022-03-26
 ### Added
 - add a `__repr__` to the `Transcriber` class
   [#145](https://github.com/NickleDave/crowsetta/pull/143).
--- src/crowsetta/__about__.py
+++ src/crowsetta/__about__.py
@@ -23,7 +23,7 @@ __title__ = "crowsetta"
 __summary__ = "A tool to work with any format for annotating vocalizations"
 __uri__ = "https://github.com/NickleDave/crowsetta"
 
-__version__ = "3.3.0"
+__version__ = "3.4.0"
 
 if base_dir is not None and os.path.exists(os.path.join(base_dir, ".commit")):
     with open(os.path.join(base_dir, ".commit")) as fp:
@@ -35,4 +35,4 @@ __author__ = "David Nicholson"
 __email__ = "nicholdav@gmail.com"
 
 __license__ = "BSD"
-__copyright__ = "2019 %s" % __author__
+__copyright__ = "2019-present %s" % __author__
