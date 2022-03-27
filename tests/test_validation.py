1c880001329a5f6a42f9ef13d2bd326932242813
David Nicholson <nickledave@users.noreply.github.com>
2022-03-27 13:02:35 -0400

    TST: add tests/test_validation.py

---
+++ tests/test_validation.py
@@ -0,0 +1,24 @@
+import pathlib
+
+import pytest
+
+import crowsetta.validation
+
+
+@pytest.mark.parametrize(
+    'file, extension, raises',
+    [
+        ('gy6or6/032312/gy6or6_baseline_230312_0808.138.cbin.not.mat', '.not.mat', False),
+        (pathlib.Path('gy6or6/032312/gy6or6_baseline_230312_0808.138.cbin.not.mat'), '.not.mat', False),
+        ('gy6or6/032312/gy6or6_baseline_230312_0808.138.cbin', '.not.mat', True),
+        (pathlib.Path('gy6or6/032312/gy6or6_baseline_230312_0808.138.cbin'), '.not.mat', True),
+        ('tests/data_for_tests/koumura/Bird0/Annotation.xml', '.xml', False),
+        (pathlib.Path('tests/data_for_tests/koumura/Bird0/Annotation.xml'), '.xml', False)
+    ]
+)
+def test_validate_ext(file, extension, raises):
+    if not raises:
+        assert crowsetta.validation.validate_ext(file, extension) is None
+    else:
+        with pytest.raises(ValueError):
+            crowsetta.validation.validate_ext(file, extension)
