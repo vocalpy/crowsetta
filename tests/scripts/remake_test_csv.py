c7b894b371360abbd0c7f3e527570dc075791dfa
David Nicholson <david@embedintel.com>
2022-04-09 10:48:31 -0400

    TST: rewrite tests/scripts/remake_test_csv.py

--- tests/scripts/remake_test_csv.py
+++ tests/scripts/remake_test_csv.py
@@ -1,4 +1,9 @@
+"""
+remakes the .csv files used for testing the `'generic-seq'` format
+"""
 from pathlib import Path
+
+import pandas as pd
 
 import crowsetta
 
@@ -6,26 +11,119 @@ HERE = Path(__file__).parent
 TEST_DATA = HERE.joinpath('..', 'data_for_tests')
 
 
-def main():
+def remake_notmat_as_generic_seq_csv():
     cbin_dir = TEST_DATA.joinpath('cbins/gy6or6/032312/')
-    notmat_list = [str(path) for path in cbin_dir.glob('*.not.mat')]
-    # below, sorted() so it's the same order on different platforms
-    notmat_list = sorted(notmat_list)
-    annot_list = crowsetta.notmat.notmat2annot(notmat_list)
-    csv_filename = TEST_DATA.joinpath('csv/gy6or6_032312.csv')
-    crowsetta.csv.annot2csv(csv_filename=csv_filename, annot=annot_list,
-                            basename=True)
+    notmat_paths = sorted(cbin_dir.glob('*.not.mat'))
+    annots = [crowsetta.formats.NotMat.from_file(notmat_path).to_annot()
+              for notmat_path in notmat_paths]
+    notmat_generic_seq = crowsetta.formats.GenericSeq(annots=annots)
+    csv_path = TEST_DATA / 'csv' / 'notmat_gy6or6_032312.csv'
+    print(
+        f'saving csv: {csv_path}'
+    )
+    notmat_generic_seq.to_csv(csv_path=csv_path, basename=True)
 
-    module = str(HERE.joinpath('example.py'))
-    config = {'module': module, 'from_file': 'example2annot'}
-    scribe = crowsetta.Transcriber(format='example', config=config)
-    mat_file = str(
-        TEST_DATA.joinpath('example_user_format', 'bird1_annotation.mat')
+
+def remake_birdsongrec_as_generic_seq_csv():
+    birdsongrec_dir = TEST_DATA / 'birdsongrec' / 'Bird0'
+    birdsongrec_xml_file = birdsongrec_dir / 'Annotation.xml'
+    birdsongrec_wavpath = birdsongrec_dir / 'Wave'
+    birdsongrec = crowsetta.formats.BirdsongRec.from_file(xml_path=birdsongrec_xml_file,
+                                                          wav_path=birdsongrec_wavpath,
+                                                          concat_seqs_into_songs=True)
+    annots = birdsongrec.to_annot()
+    generic_seq = crowsetta.formats.GenericSeq(annots=annots)
+    csv_path = TEST_DATA / 'csv' / 'birdsongrec_Bird0_Annotation.csv'
+    print(
+        f'saving csv: {csv_path}'
     )
-    csv_filename = str(
-        TEST_DATA.joinpath('csv', 'example_annotation_with_onset_inds_offset_inds_None.csv')
+    generic_seq.to_csv(csv_path=csv_path, basename=True)
+
+
+def remake_timit_phn_as_generic_seq_csv():
+    timit_kaggle_dir = TEST_DATA / 'timit_kaggle' / 'dr1-fvmh0'
+    phn_paths = sorted(timit_kaggle_dir.glob('*.phn'))
+    annots = [crowsetta.formats.Timit.from_file(phn_path).to_annot()
+              for phn_path in phn_paths]
+    timit_generic_seq = crowsetta.formats.GenericSeq(annots=annots)
+    csv_path = TEST_DATA / 'csv' / 'timit-dr1-fvmh0-phn.csv'
+    print(
+        f'saving csv: {csv_path}'
     )
-    scribe.to_csv(mat_file, csv_filename=csv_filename)
+    timit_generic_seq.to_csv(csv_path=csv_path, basename=True)
+
+
+def remake_invalid_fields_in_header_csv(source_csv_path):
+    df = pd.read_csv(source_csv_path)
+    df['invalid'] = df['label'].copy()
+
+    csv_path = TEST_DATA / 'csv' / 'invalid_fields_in_header.csv'
+    print(
+        f'saving csv: {csv_path}'
+    )
+    df.to_csv(csv_path, index=False)
+
+
+def remake_missing_fields_in_header_csv(source_csv_path):
+    df = pd.read_csv(source_csv_path)
+    df = df.drop(columns=['label'])
+
+    csv_path = TEST_DATA / 'csv' / 'missing_fields_in_header.csv'
+    print(
+        f'saving csv: {csv_path}'
+    )
+    df.to_csv(csv_path, index=False)
+
+
+def remake_onset_s_column_but_no_offset_s_column_csv(source_csv_path):
+    df = pd.read_csv(source_csv_path)
+    df = df.drop(columns=['offset_s'])
+
+    csv_path = TEST_DATA / 'csv' / 'onset_s_column_but_no_offset_s_column.csv'
+    print(
+        f'saving csv: {csv_path}'
+    )
+    df.to_csv(csv_path, index=False)
+
+
+def remake_onset_ind_column_but_no_offset_ind_column_csv(source_csv_path):
+    df = pd.read_csv(source_csv_path)
+    df = df.drop(columns=['offset_ind'])
+
+    csv_path = TEST_DATA / 'csv' / 'onset_ind_column_but_no_offset_ind_column.csv'
+    print(
+        f'saving csv: {csv_path}'
+    )
+    df.to_csv(csv_path, index=False)
+
+
+def remake_no_onset_or_offset_column_csv(source_csv_path):
+    df = pd.read_csv(source_csv_path)
+    df = df.drop(columns=['onset_s', 'offset_s'])
+
+    csv_path = TEST_DATA / 'csv' / 'no_onset_or_offset_column.csv'
+    print(
+        f'saving csv: {csv_path}'
+    )
+    df.to_csv(csv_path, index=False)
+
+
+def main():
+    print(
+        """Re-generating .csv files to use for testing `'generic-seq'` schema"""
+    )
+    remake_notmat_as_generic_seq_csv()
+    remake_birdsongrec_as_generic_seq_csv()
+    remake_timit_phn_as_generic_seq_csv()
+
+    source_csv_path = TEST_DATA / 'csv' / 'notmat_gy6or6_032312.csv'
+    remake_invalid_fields_in_header_csv(source_csv_path)
+    remake_missing_fields_in_header_csv(source_csv_path)
+    remake_onset_s_column_but_no_offset_s_column_csv(source_csv_path)
+    remake_no_onset_or_offset_column_csv(source_csv_path)
+
+    source_csv_path = TEST_DATA / 'csv' / 'timit-dr1-fvmh0-phn.csv'
+    remake_onset_ind_column_but_no_offset_ind_column_csv(source_csv_path)
 
 
 if __name__ == '__main__':
