696a4209454ed2bf54cb7f9e648c93dce8fea4f0
David Nicholson <david@embedintel.com>
2022-04-09 22:05:22 -0400

    ENH: Add formats/seq/timit.py, fixes #154

--- src/crowsetta/formats/seq/__init__.py
+++ src/crowsetta/formats/seq/__init__.py
@@ -2,10 +2,12 @@ from .birdsongrec import BirdsongRec
 from .csv import Csv
 from .generic import GenericSeq
 from .notmat import NotMat
+from .timit import Timit
 
 __all__ = [
     'BirdsongRec',
     'Csv',
     'GenericSeq',
     'NotMat',
+    'Timit',
 ]
