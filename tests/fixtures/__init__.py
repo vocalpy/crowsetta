fc39ba4df5211e11162be99e6c88717231bd419d
David Nicholson <nickledave@users.noreply.github.com>
2022-04-11 22:24:55 -0400

    TST: rename/rewrite tests/fixtures/phn.py -> timit.py
    - rewrite fixtures in fixtures/timit.py
    
    mainly so we can parametrize a fixture
    with paths, i.e. the `a_phn_path` fixture
    is parametrized with `ALL_PHN_PATHS`;
    to have access to this list outside of the fixture,
    to pass it into the parameter argument,
    needed to declare constants outside the fixtures.

--- tests/fixtures/__init__.py
+++ tests/fixtures/__init__.py
@@ -3,9 +3,9 @@ from .csv import *
 from .data import *
 from .example_user_format import *
 from .notmat import *
-from .phn import *
 from .segment import *
 from .sequence import *
 from .simple import *
 from .textgrid import *
+from .timit import *
 from .yarden import *
