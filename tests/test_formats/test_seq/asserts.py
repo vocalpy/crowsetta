73cec2e2e7abd57200335b1339eeb5f2a242e575
David Nicholson <nickledave@users.noreply.github.com>
2022-04-19 19:23:06 -0400

    TST: add tests/test_formats/test_seq/asserts.py

---
+++ tests/test_formats/test_seq/asserts.py
@@ -0,0 +1,6 @@
+def assert_rounded_correct_num_decimals(on_offset_arr, decimals):
+    __tracebackhide__ = True
+    assert all(
+        [len(str(float(boundary_s)).split('.')[-1]) <= decimals
+         for boundary_s in on_offset_arr]
+    )
