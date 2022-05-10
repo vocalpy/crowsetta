49236289d8feb602b44832033ecf4aaf56d6c2b6
David Nicholson <nickledave@users.noreply.github.com>
2022-05-10 19:44:26 -0400

    Rewrite Annotation class, fixes #149 #150
    so that it can have `seq` or `bboxes` attribute,
    but not both.
    
    - need flexibility in `__init__` to achieve this,
      so it is no longer an `attrs` class
      - the `__init__` method does validation
        and sets either `seq` or `bboxes` attribute
    - since it's no longer an `attrs` class,
      had to implement `__eq__`, `__ne__`,
      and `__repr__`
    - `seq` is no longer "optional" (in the way it was as of #149)
      and we only need one class for both `seq` and `bbox`
      annotations (as discussed in #150)

--- src/crowsetta/annotation.py
+++ src/crowsetta/annotation.py
@@ -1,15 +1,100 @@
+"""class that represents annotations from a single file"""
 from pathlib import Path
+from typing import List, Optional
 
-import attr
-from attr import validators, converters
-from attr.validators import instance_of
-
+import crowsetta
+from .bbox import BBox
 from .sequence import Sequence
+from .typing import PathLike
 
 
-@attr.s
 class Annotation:
-    """a class to represent annotations for a single file"""
-    annot_path = attr.ib(converter=Path)
-    audio_path = attr.ib(converter=converters.optional(Path), default=None)
-    seq = attr.ib(validator=validators.optional(instance_of(Sequence)), default=None)
+    """A class to represent annotations for a single file.
+    The annotations can be one of two types:
+    a single sequence, or a list of bounding boxes.
+
+    Attributes
+    ----------
+    annot_path : str, pathlib.Path
+        path to file from which annotations were loaded
+    audio_path : str, pathlib.Path
+        path to audio file that ``annot_path`` annotates.
+        Optional, default is None.
+    seq : crowsetta.Sequence
+        a sequence of annotated segments,
+        each having an onset time, offset time,
+        and label.
+    bboxes : list
+        of ``crowsetta.BBox``,
+        annotated bounding boxes,
+        each having an onset time, offset time,
+        lowest frequency, highest frequency,
+        and label.
+
+    Notes
+    -----
+    A ``crowsetta.Annotation`` can have a ``seq``
+    or ``bboxes``, but not both.
+    """
+    def __init__(self,
+                 annot_path: PathLike,
+                 audio_path: Optional[PathLike] = None,
+                 seq: Optional[Sequence] = None,
+                 bboxes: Optional[List[BBox]] = None):
+        if seq is None and bboxes is None:
+            raise ValueError(
+                'an Annotation must have either a ``seq`` or ``bboxes``'
+            )
+
+        if seq is not None and bboxes is not None:
+            raise ValueError(
+                'an Annotation can have either a ``seq``'
+                'or ``bboxes``, but not both.'
+            )
+
+        if seq:
+            if not isinstance(seq, crowsetta.Sequence):
+                raise TypeError(
+                    f'``seq`` should be a ``crowsetta.Sequence`` but was: {type(seq)}'
+                )
+            self.seq = seq
+
+        if bboxes:
+            if not isinstance(bboxes, list):
+                raise ValueError(
+                    '``bboxes`` should be a list'
+                )
+            if not all(
+                    [isinstance(bbox, BBox) for bbox in bboxes]
+            ):
+                raise ValueError(
+                    '``bboxes`` should be a list of ``crowsetta.BBox`` instances'
+                )
+            self.bboxes = bboxes
+
+        self.annot_path = Path(annot_path)
+        if audio_path:
+            self.audio_path = Path(audio_path)
+        else:
+            self.audio_path = audio_path
+
+    def __repr__(self):
+        repr_ = f'Annotation(annot_path={repr(self.annot_path)}, audio_path={repr(self.audio_path)}, '
+        if hasattr(self, 'seq'):
+            repr_ += f'seq={self.seq})'
+        elif hasattr(self, 'bboxes'):
+            repr_ += f'bboxes={self.bboxes})'
+        return repr_
+
+    def __eq__(self, other):
+        is_annot_and_audio_eq = (self.annot_path == other.annot_path and
+                                 self.audio_path == other.audio_path)
+        if hasattr(self, 'seq') and hasattr(other, 'seq'):
+            return is_annot_and_audio_eq and self.seq == other.seq
+        elif hasattr(self, 'bboxes') and hasattr(other, 'bboxes'):
+            return is_annot_and_audio_eq and self.bboxes == other.bboxes
+        else:
+            return False
+
+    def __ne__(self, other):
+        return not self == other
