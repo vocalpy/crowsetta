2e3338838dead5d95fd27bf6343ecba2348ba18d
David Nicholson <nickledave@users.noreply.github.com>
2022-05-14 22:32:33 -0400

    CLN/DEP: Remove 'csv' format, fix #171

--- src/crowsetta/formats/seq/__init__.py
+++ src/crowsetta/formats/seq/__init__.py
@@ -1,5 +1,4 @@
 from .birdsongrec import BirdsongRec
-from .csv import Csv
 from .generic import GenericSeq
 from .notmat import NotMat
 from .simple import SimpleSeq
@@ -9,7 +8,6 @@ from .yarden import SongAnnotationGUI
 
 __all__ = [
     'BirdsongRec',
-    'Csv',
     'GenericSeq',
     'NotMat',
     'SimpleSeq',
