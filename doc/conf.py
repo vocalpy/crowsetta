e29f82ea05f59d549fb3e21e2815b0a8fb47aac3
David Nicholson <nickledave@users.noreply.github.com>
2022-06-05 11:11:29 -0400

    DOC: replace 'myst_parser' with 'myst_nb' in extensions in doc/conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -39,7 +39,7 @@ release = ''
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'myst_parser',
+    'myst_nb',
     'sphinx.ext.autodoc',
     'sphinx.ext.coverage',
     'sphinx_copybutton',
