b2eabc000b2ab5b3c25ed27e373c32bf5be2f4f5
David Nicholson <nickledave@users.noreply.github.com>
2022-06-15 11:25:16 -0400

    DOC: Add module-level docstring to src/crowsetta/interface/__init__.py

--- src/crowsetta/interface/__init__.py
+++ src/crowsetta/interface/__init__.py
@@ -1,3 +1,6 @@
+"""This module declares the interface for any class that
+represents annotations loaded from a file in a specific format
+for annotating vocalizations."""
 from .base import BaseFormat
 from .bbox import BBoxLike
 from .seq import SeqLike
