eb1d2125181b647346127dc946dbcd1935e61454
David Nicholson <nickledave@users.noreply.github.com>
2022-06-15 11:25:43 -0400

    DOC: Revise docstrings in src/crowsetta/interface/seq/base.py

--- src/crowsetta/interface/seq/base.py
+++ src/crowsetta/interface/seq/base.py
@@ -1,18 +1,18 @@
-"""abstract base class for sequence-like annotation formats"""
+"""An abstract base class for sequence-like annotation formats"""
 import abc
 
 from ..base import BaseFormat
 
 
 class SeqLike(BaseFormat, abc.ABC):
-    """abstract base class defining the interface
+    """An abstract base class defining the interface
     for any annotation format
     that can be represented as a sequence of segments,
     with each segment having an onset time,
     offset time, and a label.
     """
     def to_seq(self) -> 'Union[crowsetta.Sequence, Sequence[crowsetta.Sequence]]':
-        """converts the annotation to
+        """Converts the annotation to
         a ``crowsetta.Sequence`` instance
         or a python sequence of
         ``crowsetta.Sequence``s
