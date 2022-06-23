3960ef6f7ff06d4f04be099782020420d08f9d3f
David Nicholson <NickleDave@users.noreply.github.com>
2022-06-23 10:28:14 -0400

    DOC: Change conf.py so sidebar just shows logo

--- doc/conf.py
+++ doc/conf.py
@@ -96,7 +96,9 @@ html_logo = "_static/crowsetta-primary-logo.png"
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    "logo_only": True,
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
