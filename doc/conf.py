4c696cb2da7713bf96b864e51bf49874ff20db7e
David Nicholson <NickleDave@users.noreply.github.com>
2022-06-22 22:59:15 -0400

    DOC: Add 'html_logo' to doc/conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -90,6 +90,8 @@ pygments_style = None
 #
 html_theme = 'sphinx_book_theme'
 
+html_logo = "_static/crowsetta-primary-logo.png"
+
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
