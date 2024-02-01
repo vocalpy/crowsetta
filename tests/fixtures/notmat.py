bfe56a4e0e146ac75c334b39f46e8ab88a5832f9
David Nicholson <nickledave@users.noreply.github.com>
2024-02-01 17:46:10 -0500

    Add additional fixture in tests/fixtures/notmat.py

--- tests/fixtures/notmat.py
+++ tests/fixtures/notmat.py
@@ -16,10 +16,18 @@ NOTMATS = sorted(NOTMATS_ROOT.glob("*.not.mat"))
 
 
 @pytest.fixture
-def notmat_paths(notmats_root):
+def notmat_paths():
     return NOTMATS
 
 
 @pytest.fixture(params=NOTMATS)
 def a_notmat_path(request):
     return request.param
+
+
+NOTMAT_WITH_SINGLE_ANNOTATED_SEGMENT = TEST_DATA_ROOT / 'cbins' / 'or60yw70-song-edited-to-have-single-segment' / 'or60yw70_300912_0725.437.cbin.not.mat'
+
+
+@pytest.fixture
+def notmat_with_single_annotated_segment():
+    return NOTMAT_WITH_SINGLE_ANNOTATED_SEGMENT
