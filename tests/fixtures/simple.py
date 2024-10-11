d9e1155072f490b792d8a411496dc1ea3418273b
David Nicholson <NickleDave@users.noreply.github.com>
2024-10-11 11:29:28 -0400

    Add fixture `jourjine_et_al_2023_csv_path` to fixtures/simple.py

--- tests/fixtures/simple.py
+++ tests/fixtures/simple.py
@@ -26,3 +26,11 @@ def das_csv_paths():
 @pytest.fixture(params=DAS_CSVS)
 def a_das_csv_path(request):
     return request.param
+
+
+@pytest.fixture()
+def jourjine_et_al_2023_csv_path():
+    return (
+        TEST_DATA_ROOT / "simple-csv" / "jourjine-et-al-2023" / 
+        "GO_24860x23748_ltr2_pup3_ch4_4800_m_337_295_fr1_p9_2021-10-02_12-35-01.csv"
+    )
