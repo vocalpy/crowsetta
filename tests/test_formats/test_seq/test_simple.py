6cc51e565e5c2b7634894e6b0a711334a73e541b
David Nicholson <NickleDave@users.noreply.github.com>
2024-10-11 11:29:52 -0400

    Write unit tests for new features added to SimpleSeq

--- tests/test_formats/test_seq/test_simple.py
+++ tests/test_formats/test_seq/test_simple.py
@@ -1,4 +1,6 @@
 import filecmp
+import inspect
+import tempfile
 
 import numpy as np
 import pandas as pd
@@ -112,3 +114,84 @@ def test_to_file(a_simple_csv_path, tmp_path):
     csv_path = tmp_path / a_simple_csv_path.name
     simple.to_file(annot_path=csv_path)
     assert filecmp.cmp(a_simple_csv_path, csv_path)
+
+
+def test_empty_annotations(tmpdir):
+    # test fix for https://github.com/vocalpy/crowsetta/issues/264
+    empty_df = pd.DataFrame.from_records([])
+    with tempfile.NamedTemporaryFile(dir=tmpdir, suffix=".csv") as fp:
+        empty_df.to_csv(fp.name)
+        # we need to do this all in the context manager block, because the TemporaryFile gets deleted after
+        simple = crowsetta.formats.seq.SimpleSeq.from_file(annot_path=fp.name)
+        assert isinstance(simple, crowsetta.formats.seq.SimpleSeq)
+        for attr in ("onsets_s", "offsets_s", "labels"):
+            assert np.array_equal(
+                getattr(simple, attr), np.array([])
+            )
+
+
+def test_columns_map_only_maps_columns(jourjine_et_al_2023_csv_path):
+    # test fixes for https://github.com/vocalpy/crowsetta/issues/272
+    simple = crowsetta.formats.seq.SimpleSeq.from_file(
+        annot_path=jourjine_et_al_2023_csv_path,
+        columns_map={"start_seconds": "onset_s", "stop_seconds": "offset_s"}
+    )
+    assert isinstance(simple, crowsetta.formats.seq.SimpleSeq)
+
+
+@pytest.mark.parametrize(
+        'columns_map, expected_exception',
+        [
+            # not a dict, throws a TypeError
+            ([], TypeError),
+            # not all key-value pairs are string to string
+            ({"start_seconds": "onset_s", "stop_seconds": 0}, ValueError),
+            # invalid value, not in ("onset_s", "offset_s", "label")
+            ({"start_seconds": "onset_s", "stop_seconds": "ofset_ss"}, ValueError),
+        ]
+)
+def test_columns_map_raise(
+        columns_map,
+        expected_exception,
+        jourjine_et_al_2023_csv_path
+):
+    with pytest.raises(expected_exception):
+        crowsetta.formats.seq.SimpleSeq.from_file(
+                annot_path=jourjine_et_al_2023_csv_path,
+                columns_map=columns_map,
+            )
+
+
+@pytest.mark.parametrize(
+        "default_label",
+        [
+            None,
+            'x',
+            '@'
+        ]
+)
+def test_default_label(
+    default_label,
+    jourjine_et_al_2023_csv_path
+    ):
+    # test fixes for https://github.com/vocalpy/crowsetta/issues/271
+    if default_label is None:
+        simple = crowsetta.formats.seq.SimpleSeq.from_file(
+            annot_path=jourjine_et_al_2023_csv_path,
+            columns_map={"start_seconds": "onset_s", "stop_seconds": "offset_s"}
+            # we are using default value for `default_label` here
+        )
+        sig = inspect.signature(crowsetta.SimpleSeq.from_file)
+        # replace None with the default value for default label, so assert works below
+        default_label = sig.parameters["default_label"].default
+    else:
+        simple = crowsetta.formats.seq.SimpleSeq.from_file(
+            annot_path=jourjine_et_al_2023_csv_path,
+            columns_map={"start_seconds": "onset_s", "stop_seconds": "offset_s"},
+            default_label=default_label
+        )
+
+    assert np.array_equal(
+        simple.labels, 
+        np.array([default_label] * simple.onsets_s.shape[-1])
+    )
